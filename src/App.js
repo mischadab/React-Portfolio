7e02c4ebd59165bf9d4e24d19dab4d3a03f6e389
mischadab <mischadabrowski@gmail.com>
2021-03-22 17:49:45 -0400

    navbar added to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-// import Navbar from "./components/Navbar";
 // import Footer from "./components/Footer";
 // import Wrapper from "./components/Wrapper";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <NavLink className="navbar-brand" to="/">Mischa Dabrowski</NavLink>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink to="/" className="nav-link" activeClassName="active">About</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/discover" className="nav-link" activeClassName="active">Portfolio</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/search" className="nav-link" activeClassName="active">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
 function App() {
   return (
@@ -18,9 +38,62 @@ function App() {
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
         </Wrapper>
-        <Footer />
+        <footer className="footer">
+          <span>© Copyright</span>
+        </footer>
       </div>
     </Router>
+  );
+}
+
+export default App;
+
+
+
+
+
+// simple component for 
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <NavLink className="navbar-brand" to="/">Pupster</NavLink>
+      {/* <a href="/" class="navbar-brand">Pupster</a> */}
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink to="/" className="nav-link" activeClassName="active">About</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/discover" className="nav-link" activeClassName="active">Discover</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/search" className="nav-link" activeClassName="active">Search</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+
+function App() {
+  return (
+      <div>
+        <Router>
+          <Navbar />
+          <main>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/discover" component={Discover} />
+            <Route exact path="/search" component={Search} />
+          </main>
+        </Router>
+
+        <footer className="footer">
+          <span>Pupster 2020</span>
+        </footer>
+      </div>
   );
 }
 
