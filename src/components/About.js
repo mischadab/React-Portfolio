a00891c977fc30435a49457d03745a3fe15100f2
mischadab <mischadabrowski@gmail.com>
2021-03-22 20:57:05 -0400

    about section started

---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './About.css'
+
+
+function About() {
+  return (
+    <div>
+
+      <div className="container-fluid">
+
+     
+        <h3>Let me intoduce myself!</h3>
+        <p>
+          Hi! I'm Mischa Dabrowski. I'm a junior front-end web developer, with a background in graphic design and photography. 
+        </p>
+        <p>
+          Aside from coding, here are some other activities that I love to do:
+          <ul>
+            <li>Playing games</li>
+            <li>Tinkering with film cameras</li>
+            <li>Drawing</li>
+            <li>Rollerskating</li>
+          </ul>
+        </p>
+       
+
+      </div>
+
+    </div>
+  );
+}
+
+export default About;
