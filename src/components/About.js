3c51f74c295e7df5c9f26122ddd311c308f3e6a8
mischadab <mischadabrowski@gmail.com>
2021-03-22 22:45:09 -0400

    added more to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,32 +1,45 @@
 import React from "react";
 import './About.css'
+import headshot from '../assets/headshot.jpg'
 
 
 function About() {
   return (
-    <div>
+    // <div>
 
-      <div className="container-fluid">
+    <div className="container">
 
-     
-        <h3>Let me intoduce myself!</h3>
-        <p>
-          Hi! I'm Mischa Dabrowski. I'm a junior front-end web developer, with a background in graphic design and photography. 
-        </p>
-        <p>
-          Aside from coding, here are some other activities that I love to do:
+      <div class="row">
+
+        <div class="col-md-auto">
+          <h3>Let me introduce myself!</h3>
+          <p>
+            Hi! I'm Mischa Dabrowski. I'm a junior front-end web developer, with a background in graphic design and photography.
+          </p>
+          <p>
+            Aside from coding, here are some other activities that I love to do:
           <ul>
-            <li>Playing games</li>
-            <li>Tinkering with film cameras</li>
-            <li>Drawing</li>
-            <li>Rollerskating</li>
-          </ul>
-        </p>
-       
+              <li>Playing games</li>
+              <li>Tinkering with film cameras</li>
+              <li>Drawing</li>
+              <li>Rollerskating</li>
+            </ul>
+          </p>
+        </div>
+
+        <div class="col-md-auto">
+          <img className="img-fluid" src={headshot} alt="Mischa's headshot" />
+        </div>
 
       </div>
 
+
+
+
+
     </div>
+
+    // </div>
   );
 }
 
