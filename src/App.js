7f6f66c09915d0d26a0581bfacf588bc08720578
mischadab <mischadabrowski@gmail.com>
2021-03-25 20:06:20 -0400

    new nav bar

--- src/App.js
+++ src/App.js
@@ -9,22 +9,32 @@ import Wrapper from './components/Wrapper'
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <NavLink className="navbar-brand" to="/">Mischa Dabrowski</NavLink>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink to="/" className="nav-link" activeClassName="active">About</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/portfolio" className="nav-link" activeClassName="active">Portfolio</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/contact" className="nav-link" activeClassName="active">Contact</NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    //   <NavLink className="navbar-brand" to="/">Mischa Dabrowski</NavLink>
+    //   <div>
+    //     <ul className="navbar-nav">
+    //       <li className="nav-item">
+    //         <NavLink to="/" className="nav-link" activeClassName="active">ABOUT</NavLink>
+    //       </li>
+    //       <li className="nav-item">
+    //         <NavLink to="/portfolio" className="nav-link" activeClassName="active">PORTFOLIO</NavLink>
+    //       </li>
+    //       <li className="nav-item">
+    //         <NavLink to="/contact" className="nav-link" activeClassName="active">CONTACT</NavLink>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </nav>
+
+    <div>
+      <h1 className="title">Mischa Dabrowski</h1>
+      <nav>
+      <NavLink to="/" className="navv"> ABOUT </NavLink>    
+      <NavLink to="portfolio" className="navv"> PORTFOLIO </NavLink>     
+      <NavLink to="/" className="navv"> CONTACT </NavLink>    
+      </nav>
+    </div>
+
   );
 }
 
@@ -33,10 +43,10 @@ function App() {
 
       <div className="quack">
       <Router>
-        <Navbar />
+     
 
         <Wrapper>
-       
+          <Navbar />
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
