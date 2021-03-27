6c92d870f6a916275345ff2c7c70e4ecad0e6f94
mischadab <mischadabrowski@gmail.com>
2021-03-27 15:54:17 -0400

    added card template to portfolio

---
+++ src/components/Portfolio.js
@@ -0,0 +1,97 @@
+import './Portfolio.css'
+
+function Portfolio() {
+    return (
+        <div>
+            <div className="row">
+                <div class="card mb-3 cardsize">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3 cardsize">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div class="card mb-3 cardsize">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3 cardsize">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row">
+                <div class="card mb-3 cardsize">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3 cardsize">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="..." alt="..." />
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Portfolio;
